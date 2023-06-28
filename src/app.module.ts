7a8641b79b5d1ab388bf704c662dd764ea566f9c
balzer <antonbaltser@yahoo.com>
2023-06-28 13:44:48 +0000

    authService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsController } from './posts/posts.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController, PostsController],
   providers: [AppService],
 })
