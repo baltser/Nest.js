60e16894f82983164e52a31ecc2cdefa7cb3fa33
balzer <antonbaltser@yahoo.com>
2023-07-02 13:51:43 +0000

    configurateService

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { PostsController } from './posts/posts.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from "@nestjs/config";
+import configurations from "./configurations";
+import{ SequelizeModule } from "@nestjs/sequelize";
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    load: [configurations]
+  }), AuthModule, UsersModule],
   controllers: [AppController, PostsController],
   providers: [AppService],
 })
